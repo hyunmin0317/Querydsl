dc10a8de0b955fbf54a0b1474bd8b304fe518efc
hyunmin0317 <choihm9903@naver.com>
2024-03-24 16:59:44 +0900

    test: QueryDSL 설정 테스트 (#1)

---
+++ src/test/java/study/querydsl/entity/HelloTest.java
@@ -0,0 +1,38 @@
+package study.querydsl.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static study.querydsl.entity.QHello.hello;
+
+@SpringBootTest
+@Transactional
+class HelloTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    JPAQueryFactory query;
+
+    @Test
+    public void contextLoads() throws Exception {
+        //given
+        Hello h = new Hello();
+        em.persist(h);
+
+        //when
+        Hello result = query
+                .selectFrom(hello)
+                .fetchOne();
+
+        //then
+        Assertions.assertThat(result).isEqualTo(h);
+        Assertions.assertThat(result.getId()).isEqualTo(h.getId());
+    }
+}

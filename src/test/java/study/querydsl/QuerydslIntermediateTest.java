f1d5efd0285cae40e10c88470958071ddc0b46ab
hyunmin0317 <choihm9903@naver.com>
2024-03-25 15:42:19 +0900

    feat: 프로젝션과 결과 반환 - 기본 (#4)

---
+++ src/test/java/study/querydsl/QuerydslIntermediateTest.java
@@ -0,0 +1,54 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import java.util.List;
+
+import static study.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class QuerydslIntermediateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    JPAQueryFactory query;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void simpleProjection() throws Exception {
+        List<String> result = query
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        result.forEach(System.out::println);
+    }
+}

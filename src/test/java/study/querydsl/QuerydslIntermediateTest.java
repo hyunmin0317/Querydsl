d89a5c40be1d32c98b7989599416beb63832f55a
hyunmin0317 <choihm9903@naver.com>
2024-03-25 17:29:05 +0900

    feat: 동적 쿼리 - BooleanBuilder 사용 (#4)

--- src/test/java/study/querydsl/QuerydslIntermediateTest.java
+++ src/test/java/study/querydsl/QuerydslIntermediateTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
@@ -20,6 +21,7 @@ import study.querydsl.entity.Team;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
@@ -156,5 +158,26 @@ public class QuerydslIntermediateTest {
                 .fetch();
 
         result.forEach(System.out::println);
+    }
+
+    @Test
+    public void dynamicQueryBooleanBuilder() throws Exception {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null)
+            builder.and(member.username.eq(usernameCond));
+        if (ageCond != null)
+            builder.and(member.age.eq(ageCond));
+        return query
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }

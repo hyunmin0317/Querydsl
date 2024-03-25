3590d323169f93ae41448282bb1303c0cf19164f
hyunmin0317 <choihm9903@naver.com>
2024-03-25 17:39:59 +0900

    feat: 동적 쿼리 - Where 다중 파라미터 사용 (#4)

--- src/test/java/study/querydsl/QuerydslIntermediateTest.java
+++ src/test/java/study/querydsl/QuerydslIntermediateTest.java
@@ -4,6 +4,7 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -169,6 +170,15 @@ public class QuerydslIntermediateTest {
         assertThat(result.size()).isEqualTo(1);
     }
 
+    @Test
+    public void dynamicQueryWhereParam() throws Exception {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember2(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
     private List<Member> searchMember1(String usernameCond, Integer ageCond) {
         BooleanBuilder builder = new BooleanBuilder();
         if (usernameCond != null)
@@ -179,5 +189,24 @@ public class QuerydslIntermediateTest {
                 .selectFrom(member)
                 .where(builder)
                 .fetch();
+    }
+
+    private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+        return query
+                .selectFrom(member)
+                .where(usernameEq(usernameCond), ageEq(ageCond))
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String usernameCond) {
+        return usernameCond != null ? member.username.eq(usernameCond) : null;
+    }
+
+    private BooleanExpression ageEq(Integer ageCond) {
+        return ageCond != null ? member.age.eq(ageCond) : null;
+    }
+
+    private BooleanExpression allEq(String usernameCond, Integer ageCond) {
+        return usernameEq(usernameCond).and(ageEq(ageCond));
     }
 }

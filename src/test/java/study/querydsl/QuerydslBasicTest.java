f63253ccab28d150b111bd14c5c6f9d10c69d65f
hyunmin0317 <choihm9903@naver.com>
2024-03-25 15:29:11 +0900

    feat: Case 문 (#3)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -398,6 +399,33 @@ public class QuerydslBasicTest {
                                 .from(memberSub))
                 .from(member)
                 .fetch();
+
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    public void basicCase() throws Exception {
+        List<String> result = query
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    public void complexCase() throws Exception {
+        List<String> result = query
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
         result.forEach(System.out::println);
     }
 }
